f7f93e6d489cbddddc7dbd9b904c62ee08dce3e0
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-02-06 23:26:45 -0500

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,47 +1,29 @@
-import java.util.Arrays;
-
+import java.util.ArrayList;
+import java.util.*;
 
 public class BubbleSort
 {
-
-    
-    static void bubbleSort(int[] lst) {
-
-    int n = lst.length;
-    boolean swapped; 
-    do
-    {
-        swapped = false;
-        for(int i = 0; i < n-1; i++)
-        {
-           if(lst[i] > lst[i+1]) 
-           {
-               
-               int temp = lst[i]; 
-               lst[i] = lst[i+1];
-               lst[i+1] = temp;
-               swapped = true;
-           }   
-                 
-        }
-            
-         
-            
-    } while (swapped == true);
-   
-    //debugging 
-    System.out.println("works");
- 
-    // result     
-    System.out.println(Arrays.toString(lst));
-    }
-    
-    
-
-    
-
-    public static void main(String[] args) {
-        int[] lst = {4,9,7,11,38,6,5};
-        bubbleSort (lst);
-    }
+	public static void main(String[] args) {
+		int [] lst  = {9, 1, 3, 9, 5, 1, 9, 0};
+		bubbleSort(lst);
+	
+	}
+	
+	static void bubbleSort(int[] lst){
+	    int i;
+		int j;
+		int n = lst.length;
+	 
+	    for (i = 0; i < n; i++){
+		   for(j = 0; j < n-1; j++){
+    	        if (lst[j] > lst[j+1]){
+                    int temp = lst[j];
+                    lst[j] = lst[j+1];
+                    lst[j+1] = temp;
+    		    }
+		    }
+	    }
+        System.out.println(Arrays.toString(lst));
+	}
 }
+
