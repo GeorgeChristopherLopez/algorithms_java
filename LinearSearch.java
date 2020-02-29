825f1dfff275bf1633798dfc681de050e7654f8b
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-02-28 23:58:08 -0500

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,25 @@
+public class LinearSearch
+{
+
+   
+    public static void main(String[] args) {
+        int [] lst = {37,9,7,5,4,6};
+       linearSearch (lst, 37);
+    }
+    
+     static void linearSearch(int[] lst, int search)
+    {
+       
+        for(int i = 0; i< lst.length -1; i++ )
+        {
+            if(lst[i]== search)
+            {
+              System.out.println ("Found " + search +". It is at index " + i +".");
+              return;
+            } 
+              
+            
+        }
+        System.out.println ("Didnt find " + search +". It is not in the array.");
+    }
+}
